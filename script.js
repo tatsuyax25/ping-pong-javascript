5f1d9290781eb4ab2d2409fca1f6da6478cd6855
Miguel Urena <urena_miguel82@outlook.com>
2023-01-09 15:52:11 -0800

    JavaScript set up pt. 2

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ import Ball from "./Ball.js";
 
 const ball = new Ball(document.getElementById("ball"))
 
+let lastTime
 function update(time) {
+    if (lastTime != null) {
+        const delta = time - lastTime
+        // Update code
+        ball.update(delta)
+    }
 
+    lastTime = time
+    window.requestAnimationFrame(update)
 }
 
 window.requestAnimationFrame(update)
