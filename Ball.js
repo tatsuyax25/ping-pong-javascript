e2048a2d5ad71845276f9d460d378f07c2878ec4
Miguel Urena <urena_miguel82@outlook.com>
2023-01-10 15:27:20 -0800

    Movement for ball

--- Ball.js
+++ Ball.js
@@ -2,4 +2,16 @@ export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
     }
+
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"))
+    }
+
+    set x(value) {
+        this.ballElem.style.setProperty("--x", value)
+    }
+
+    update(delta) {
+        this.x = 5
+    }
 }
