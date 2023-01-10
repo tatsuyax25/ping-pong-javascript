5f5002751003e7f76f39efab5e1737b6d832aa79
Miguel Urena <urena_miguel82@outlook.com>
2023-01-10 15:38:11 -0800

    Ball movement pt 3

--- Ball.js
+++ Ball.js
@@ -1,6 +1,7 @@
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem
+        this.reset()
     }
 
     get x() {
@@ -19,8 +20,22 @@ export default class Ball {
         this.ballElem.style.setProperty("--y", value)
     }
 
+    reset() {
+        this.x = 50
+        this.y = 50
+        this.direction = { x: 0 }
+        while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9) {
+            const heading = randomNumberBetween(0, 2 * Math.PI)
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading) }
+        }
+    }
+
     update(delta) {
         this.x = 5
         this.y = 15
     }
+}
+
+function randomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min
 }
