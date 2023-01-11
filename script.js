58461b13a6633edc2c4bec9189ab49e22e70527c
Miguel Urena <urena_miguel82@outlook.com>
2023-01-11 15:11:05 -0800

    Create Paddle.js for paddle

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // Game runs in an Update Loop
 import Ball from "./Ball.js";
+import Paddle from "./Paddle.js";
 
 const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new Paddle(document.getElementById("player-paddle"))
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
 
 let lastTime
 function update(time) {
     if (lastTime != null) {
         const delta = time - lastTime
         // Update code
-        ball.update(delta)
+        // ball.update(delta)
     }
 
     lastTime = time
