a8912c425fba3cee518191fe9f842e9d38d059e2
Miguel Urena <urena_miguel82@outlook.com>
2023-01-10 16:19:13 -0800

    Ball movement pt 6

--- Ball.js
+++ Ball.js
@@ -1,4 +1,5 @@
-const INITIAL_VELOCITY = .025
+const INITIAL_VELOCITY = 0.025
+const VELOCITY_INCREASE = 0.00001
 
 export default class Ball {
     constructor(ballElem) {
@@ -40,6 +41,7 @@ export default class Ball {
     update(delta) {
         this.x += this.direction.x * this.velocity * delta
         this.y += this.direction.x * this.velocity * delta
+        this.velocity += VELOCITY_INCREASE * delta
         const rect = this.rect()
 
         if (rect.bottom >= window.innerHeight || rect.top <= 0) {
