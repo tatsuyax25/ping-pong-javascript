2b290f28e95199d9ef65721593d4bec6c45807c3
Miguel Urena <urena_miguel82@outlook.com>
2023-01-16 15:05:30 -0800

    Game Logic pt 3

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ import Paddle from "./Paddle.js";
 const ball = new Ball(document.getElementById("ball"))
 const playerPaddle = new Paddle(document.getElementById("player-paddle"))
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem = document.getElementById("player-score")
+const computerScoreElem = document.getElementById("computer-score")
 
 let lastTime
 function update(time) {
@@ -14,9 +16,7 @@ function update(time) {
         // ball.update(delta)
         computerPaddle.update(delta, ball.y)
 
-        if (isLose()) {
-            console.log("lose")
-        }
+        if (isLose()) handleLose()
     }
 
     lastTime = time
@@ -26,6 +26,17 @@ function update(time) {
 function isLose() {
     const rect = ball.rect()
     return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+function handleLose() {
+    const rect = ball.rect()
+    if (rect.right >= window.innerWidth) {
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    } else {
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+    }
+    ball.reset()
+    computerPaddle.reset()
 }
 
 document.addEventListener("mousemove", e => {
